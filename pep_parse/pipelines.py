62b7d0e793ea023c4c82d632582a26f991070043
Igor Mogilin <endless8space@gmail.com>
2025-07-10 04:43:29 +0300

    First review

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,33 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from collections import defaultdict
+from pathlib import Path
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+from pep_parse.settings import BASE_DIR, RESULTS_DIR, TIMESTAMP
 
 
 class PepParsePipeline:
+    def open_spider(self, spider):
+        """Инициализация счетчика статистики."""
+        self.status_count = defaultdict(int)
+        self.total_peps = 0
+
     def process_item(self, item, spider):
+        """Сбор статистики статусов."""
+        status = item.get('status')
+        if status:
+            self.status_count[status] += 1
+            self.total_peps += 1
         return item
+
+    def close_spider(self, spider):
+        """Запись данных статистки в файл."""
+        file_name_stamp = f'{RESULTS_DIR}/status_summary_{TIMESTAMP}.csv'
+        output_file_name = BASE_DIR / Path(file_name_stamp)
+        with open(
+            output_file_name,
+            mode='w',
+            encoding='utf-8'
+        ) as f:
+            f.write('Статус,Количество\n')
+            for status, count in sorted(self.status_count.items()):
+                f.write(f'{status},{count}\n')
+            f.write(f'Total,{self.total_peps}\n')
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,35 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
+from pep_parse.settings import ALLOWED_DOMAINS, START_URLS
+
 
 class PepSpider(scrapy.Spider):
+    """Паук для сбора информации о PEP."""
     name = 'pep'
-    allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    allowed_domains = ALLOWED_DOMAINS
+    start_urls = START_URLS
 
     def parse(self, response):
-        pass
+        """Обработка главной страницы со списком PEP."""
+        pep_links = [url for url in response.xpath(
+            '//table[contains(@class, "pep-zero-table")]//tr/td[2]/a/@href'
+        ).getall()]
+        for pep_link in pep_links:
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        """Обработка страницы отдельного PEP."""
+        pep_number = response.css(
+            'h1.page-title::text'
+        ).re_first(r'PEP\s+(\d{1,4})')
+        pep_name = response.css(
+            'h1.page-title::text'
+        ).re_first(r'PEP \d+ – (.+)')
+        status = response.css('dt:contains("Status") + dd abbr::text').get()
+        data = {
+            'number': pep_number,
+            'name': pep_name,
+            'status': status,
+        }
+        yield PepParseItem(data)
